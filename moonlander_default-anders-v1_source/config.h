356d155dcd56508f88b19e4ea423c0186ece316b
= <=>
2023-01-24 12:27:21 -0500

    moonlander layout

---
+++ moonlander_default-anders-v1_source/config.h
@@ -0,0 +1,17 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#define ORYX_CONFIGURATOR
+#define IGNORE_MOD_TAP_INTERRUPT
+#undef TAPPING_TERM
+#define TAPPING_TERM 400
+
+#define USB_SUSPEND_WAKEUP_DELAY 0
+#define FIRMWARE_VERSION u8"BPbEd/rJjjn"
+#define RAW_USAGE_PAGE 0xFF60
+#define RAW_USAGE_ID 0x61
+#define LAYER_STATE_8BIT
+
+#define RGB_MATRIX_STARTUP_SPD 60
+
