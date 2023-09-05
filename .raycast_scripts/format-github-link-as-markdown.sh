6e6c2ee5ebf76f92d0d6a3cb24e02786256004b1
= <=>
2023-09-05 16:16:13 -0400

    short link > long link

---
+++ .raycast_scripts/format-github-link-as-markdown.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Format GitHub Link as Markdown
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.description takes an issue or PR URL and turns it into a markdown link
+# @raycast.author dataders
+# @raycast.authorURL https://raycast.com/dataders
+
+# echo "Hello World!"
+
+# url example https://github.com/dbt-labs/dbt-docs/pull/7701
+
+url=$(pbpaste)
+pr_number=${url##*/}
+
+url_without_base=${url#https://github.com/dbt-labs/}
+repo_name=${url_without_base%%/*}
+
+echo "[$repo_name#$pr_number]($url)" | pbcopy
+echo Copied "[$repo_name#$pr_number]($url)"
