244043ec1dbcc5d4b8a15e088c76a645507038c1
= <=>
2024-10-16 11:00:15 -0400

    tarmak v3

--- moonlander_default-anders-v1_source/config.h
+++ moonlander_default-anders-v1_source/config.h
@@ -3,11 +3,8 @@
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
 
-#define ORYX_CONFIGURATOR
 #define USB_SUSPEND_WAKEUP_DELAY 0
-#define FIRMWARE_VERSION u8"aPEmv/RD6Wl"
-#define RAW_USAGE_PAGE 0xFF60
-#define RAW_USAGE_ID 0x61
+#define SERIAL_NUMBER "aPEmv/Yawwy"
 #define LAYER_STATE_8BIT
 
 #define RGB_MATRIX_STARTUP_SPD 60
