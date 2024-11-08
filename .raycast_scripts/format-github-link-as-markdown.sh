55f348f7c2766490eafaed8e198d2742b02a2d46
= <=>
2024-11-08 11:24:17 -0500

    works on all GH links

--- .raycast_scripts/format-github-link-as-markdown.sh
+++ .raycast_scripts/format-github-link-as-markdown.sh
@@ -13,15 +13,20 @@
 # @raycast.author dataders
 # @raycast.authorURL https://raycast.com/dataders
 
-# echo "Hello World!"
 
-# url example https://github.com/dbt-labs/dbt-docs/pull/7701
-
+# Get the URL from the clipboard
 url=$(pbpaste)
+
+# get required components
 pr_number=${url##*/}
+url_without_base=${url#https://github.com/}
+org_name=${url_without_base%%/*}
+repo_name=${url_without_base#*/}; repo_name=${repo_name%%/*}
 
-url_without_base=${url#https://github.com/dbt-labs/}
-repo_name=${url_without_base%%/*}
-
-echo "[$repo_name#$pr_number]($url)" | pbcopy
-# echo Copied "[$repo_name#$pr_number]($url)"
+# format as markdown link
+# exclude org name if it's dbt-labs
+if [ "$org_name" == "dbt-labs" ]; then
+    echo "[$repo_name#$pr_number]($url)" | pbcopy
+else
+    echo "[$org_name/$repo_name#$pr_number]($url)" | pbcopy
+fi
